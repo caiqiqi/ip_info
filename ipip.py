f844aea8fda98fd2209ddb45b63d71a6fff496d7
caiqiqi <77caikiki@gmail.com>
2017-07-07 00:16:06 +0800

    update headers

--- ipip.py
+++ ipip.py
@@ -17,9 +17,7 @@ xpath_details = "//td[2]"
 header = {
 'Host':"www.ipip.net",
 'User-Agent':"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:50.0) Gecko/20100101 Firefox/50.0",
-'Accept':"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-'Accept-Language':"zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
-'Accept-Encoding':"gzip, deflate",
+#'Accept':"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
 'Referer':"http://www.ipip.net/",
 'Connection':"keep-alive",
 'Content-Type':"application/x-www-form-urlencoded"
